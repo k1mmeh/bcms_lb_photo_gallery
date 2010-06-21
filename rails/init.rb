79fa4326d71cedc7ebf8501f051aa354c6c106b3
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2010-06-21 23:23:30 +1200

    Work-around for portlet helper bug - copies helper to core portlet helpers

--- rails/init.rb
+++ rails/init.rb
@@ -2,3 +2,4 @@ gem_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Cms.add_to_rails_paths gem_root
 Cms.add_generator_paths gem_root, "db/migrate/[0-9]*_*.rb"
 Cms.add_generator_paths gem_root, "public/bcms/lb_photo_gallery/**/*"
+Cms.add_generator_paths gem_root, "app/portlets/helpers/*"
