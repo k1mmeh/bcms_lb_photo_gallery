b7f18017921e9e5a1f1d0bb3aa87ba87f39e1241
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2011-01-10 21:09:21 +1300

    Adding published_date to file upload

--- app/portlets/photo_gallery_portlet.rb
+++ app/portlets/photo_gallery_portlet.rb
@@ -1,13 +1,13 @@
 class PhotoGalleryPortlet < Portlet
 
   # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
-  enable_template_editor false
+  enable_template_editor true
      
   def render
     per_page = @portlet.per_page.to_i > 0 ? @portlet.per_page.to_i : 10
     page = params[:ppage].to_i > 0 ? params[:ppage].to_i : 1
     @section = Section.find_by_id(@portlet.section_id.to_i) || Section.root
-    @photos = ThumbnailedImage.by_section(@section).paginate(:per_page => per_page, :page => page, :order => 'file_blocks.created_at DESC')
+    @photos = ThumbnailedImage.by_section(@section).paginate(:per_page => per_page, :page => page, :order => 'file_blocks.published_date ASC, file_blocks.created_at DESC')
   end
     
 end
--- db/migrate/20100330093918_create_thumbnailed_images.rb
+++ db/migrate/20100330093918_create_thumbnailed_images.rb
@@ -1,11 +1,17 @@
 class CreateThumbnailedImages < ActiveRecord::Migration
   def self.up
     add_column(:file_blocks, :description, :text)
+    add_column(:file_block_versions, :description, :text)
+    add_column(:file_blocks, :published_date, :date)
+    add_column(:file_block_versions, :published_date, :date)
     ContentType.create!(:name => "ThumbnailedImage", :group_name => "LB Photo Gallery")
   end
 
   def self.down
     remove_column(:file_blocks, :description)
+    remove_column(:file_block_versions, :description)
+    remove_column(:file_blocks, :published_date)
+    remove_column(:file_block_versions, :published_date)
     ContentType.delete_all(['name = ?', 'ThumbnailedImage'])
     CategoryType.all(:conditions => ['name = ?', 'Thumbnailed Image']).each(&:destroy)
   end
