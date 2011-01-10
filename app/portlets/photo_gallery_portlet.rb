288870c837dc1d1c130cbe1075dba10251ae2f59
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2011-01-10 21:21:15 +1300

    Adding published_date to file upload

--- app/portlets/photo_gallery_portlet.rb
+++ app/portlets/photo_gallery_portlet.rb
@@ -7,7 +7,7 @@ class PhotoGalleryPortlet < Portlet
     per_page = @portlet.per_page.to_i > 0 ? @portlet.per_page.to_i : 10
     page = params[:ppage].to_i > 0 ? params[:ppage].to_i : 1
     @section = Section.find_by_id(@portlet.section_id.to_i) || Section.root
-    @photos = ThumbnailedImage.by_section(@section).paginate(:per_page => per_page, :page => page, :order => 'file_blocks.published_date ASC, file_blocks.created_at DESC')
+    @photos = ThumbnailedImage.by_section(@section).paginate(:per_page => per_page, :page => page, :order => 'file_blocks.published_date DESC NULLS FIRST, file_blocks.created_at DESC')
   end
     
 end
